d78a599507b5d42517035a0a852297ebf4d7267b
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-01-26 17:48:22 +0530

    Latch concept for Daemon Thread

--- virtual-thread-playground/src/main/java/org/example/sec01/InboundOutboundTaskDemo.java
+++ virtual-thread-playground/src/main/java/org/example/sec01/InboundOutboundTaskDemo.java
@@ -1,8 +1,10 @@
 package org.example.sec01;
 
+import java.util.concurrent.CountDownLatch;
+
 public class InboundOutboundTaskDemo {
     private static final int MAX_PLATFORM = 10;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         platformThreadDemo3();
     }
     private static void  platformThreadDemo1() {
@@ -22,12 +24,17 @@ public class InboundOutboundTaskDemo {
         }
     }
 
-    private static void  platformThreadDemo3() {
+    private static void  platformThreadDemo3() throws InterruptedException {
+        var latch = new CountDownLatch(MAX_PLATFORM);
         Thread.Builder.OfPlatform builder = Thread.ofPlatform().daemon().name("daemon-", 1);
         for (int i =0; i<MAX_PLATFORM; i++) {
             int j = i;
-            Thread thread = builder.unstarted( () -> Task.inIntesive(j));
+            Thread thread = builder.unstarted( () -> {
+                Task.inIntesive(j);
+                latch.countDown();
+            });
             thread.start();
         }
+        latch.await();
     }
 }
