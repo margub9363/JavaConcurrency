cd9b85f91b459efbd905846c14e3be3ddd191681
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-01-26 17:39:56 +0530

    Introducing Daemon Threads

--- virtual-thread-playground/src/main/java/org/example/sec01/InboundOutboundTaskDemo.java
+++ virtual-thread-playground/src/main/java/org/example/sec01/InboundOutboundTaskDemo.java
@@ -3,7 +3,7 @@ package org.example.sec01;
 public class InboundOutboundTaskDemo {
     private static final int MAX_PLATFORM = 10;
     public static void main(String[] args) {
-        platformThreadDemo2();
+        platformThreadDemo3();
     }
     private static void  platformThreadDemo1() {
         for (int i =0; i<MAX_PLATFORM; i++) {
@@ -15,6 +15,15 @@ public class InboundOutboundTaskDemo {
 
     private static void  platformThreadDemo2() {
         Thread.Builder.OfPlatform builder = Thread.ofPlatform().name("rahman", 1);
+        for (int i =0; i<MAX_PLATFORM; i++) {
+            int j = i;
+            Thread thread = builder.unstarted( () -> Task.inIntesive(j));
+            thread.start();
+        }
+    }
+
+    private static void  platformThreadDemo3() {
+        Thread.Builder.OfPlatform builder = Thread.ofPlatform().daemon().name("daemon-", 1);
         for (int i =0; i<MAX_PLATFORM; i++) {
             int j = i;
             Thread thread = builder.unstarted( () -> Task.inIntesive(j));
