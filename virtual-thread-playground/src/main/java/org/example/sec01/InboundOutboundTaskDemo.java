43293e4e39b641432e41f9a53763efad4e66a47c
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-01-26 18:03:49 +0530

    Applying latch and giving name to virtual threads

--- virtual-thread-playground/src/main/java/org/example/sec01/InboundOutboundTaskDemo.java
+++ virtual-thread-playground/src/main/java/org/example/sec01/InboundOutboundTaskDemo.java
@@ -40,12 +40,17 @@ public class InboundOutboundTaskDemo {
     }
 
 //To create virtual thread using Thread.Builder
-    private static void  virtualThreadDemo() {
-        var builder = Thread.ofVirtual();
+    private static void  virtualThreadDemo() throws InterruptedException {
+        var latch = new CountDownLatch(MAX_VIRTUAL);
+        var builder = Thread.ofVirtual().name("virtual-",1);
         for (int i =0; i<MAX_VIRTUAL; i++) {
             int j = i;
-            Thread thread = builder.unstarted( () -> Task.inIntesive(j));
+            Thread thread = builder.unstarted( () -> {
+                Task.inIntesive(j);
+                latch.countDown();
+            });
             thread.start();
         }
+        latch.await();
     }
 }
