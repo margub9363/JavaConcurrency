c3e2a4998499367da9ad8c3970519f3e09bd8651
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-01-26 17:31:43 +0530

    Changing the name of the thread

--- virtual-thread-playground/src/main/java/org/example/sec01/InboundOutboundTaskDemo.java
+++ virtual-thread-playground/src/main/java/org/example/sec01/InboundOutboundTaskDemo.java
@@ -3,12 +3,21 @@ package org.example.sec01;
 public class InboundOutboundTaskDemo {
     private static final int MAX_PLATFORM = 10;
     public static void main(String[] args) {
-        platformThreadDemo();
+        platformThreadDemo2();
     }
-    private static void  platformThreadDemo() {
+    private static void  platformThreadDemo1() {
         for (int i =0; i<MAX_PLATFORM; i++) {
             int j = i;
             Thread thread = new Thread( () -> Task.inIntesive(j));
+            thread.start();
+        }
+    }
+
+    private static void  platformThreadDemo2() {
+        Thread.Builder.OfPlatform builder = Thread.ofPlatform().name("rahman", 1);
+        for (int i =0; i<MAX_PLATFORM; i++) {
+            int j = i;
+            Thread thread = builder.unstarted( () -> Task.inIntesive(j));
             thread.start();
         }
     }
