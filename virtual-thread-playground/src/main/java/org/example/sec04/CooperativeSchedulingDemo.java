5bdd3abbbdac58ca89c5b714204824aafe4d4b07
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-07-29 10:42:39 +0530

    Demo of Preemptive and Cooperative scheduling

--- virtual-thread-playground/src/main/java/org/example/sec04/CooperativeSchedulingDemo.java
+++ virtual-thread-playground/src/main/java/org/example/sec04/CooperativeSchedulingDemo.java
@@ -9,10 +9,13 @@ import java.time.Duration;
 
 public class CooperativeSchedulingDemo {
     public static final Logger log = LoggerFactory.getLogger(CooperativeSchedulingDemo.class);
+//case 1
+    // Here from line 16 to 34 premptive scheduling is happening because we are running two threads and the task 1 is getting performed
+    // and then context switching happened and the other task got executed.
 
-    public static void main(String[] args) {
+  /*  public static void main(String[] args) {
+
         var builder = Thread.ofVirtual();
-//        here we will be performing an operation on virtual thread that means cooperative scheduling will be done.
         var t1 = builder.unstarted(() -> demo(1));
         var t2 = builder.unstarted(() -> demo(2));
 
@@ -22,11 +25,64 @@ public class CooperativeSchedulingDemo {
         CommonUtils.sleep(Duration.ofSeconds(5));
     }
 
-    private static void demo (int threadNumber){
-        log.info("thread-{} started",threadNumber);
+    private static void demo(int threadNumber) {
+        log.info("thread-{} started", threadNumber);
         for (int i = 0; i < 10; i++) {
-            log.info("thread-{} is pringting {}. Thread {}",threadNumber,i,Thread.currentThread());
+            log.info("thread-{} is pringting {}. Thread {}", threadNumber, i, Thread.currentThread());
         }
-        log.info("thread-{} ended",threadNumber);
+        log.info("thread-{} ended", threadNumber);
+    } */
+
+//    case 2 line no 39 to 62
+    // To demo Cooperative scheduling we have to limit our cpu resource ie to run the program with 1 thread and 1 parallelism
+//If we run this program we will see that since we are allowing only one thread so thread 1 will execute completely then the request will be given to thread 2
+  /*  static {
+        System.setProperty("jdk.virtualThread.Scheduler.parallelism","1");
+        System.setProperty("jdk.virtualThread.Scheduler.maxPoolSize","1");
+    }
+
+    public static void main(String[] args) {
+        var builder = Thread.ofVirtual();
+        var t1 = builder.unstarted(() -> demo(1));
+        var t2 = builder.unstarted(() -> demo(2));
+
+        t1.start();
+        t2.start();
+
+        CommonUtils.sleep(Duration.ofSeconds(5));
+    }
+
+    private static void demo(int threadNumber) {
+        log.info("thread-{} started", threadNumber);
+        for (int i = 0; i < 10; i++) {
+            log.info("thread-{} is pringting {}. Thread {}", threadNumber, i, Thread.currentThread());
+        }
+        log.info("thread-{} ended", threadNumber);
+    }*/
+
+//    case3
+        static {
+        System.setProperty("jdk.virtualThread.Scheduler.parallelism","1");
+        System.setProperty("jdk.virtualThread.Scheduler.maxPoolSize","1");
+    }
+
+    public static void main(String[] args) {
+        var builder = Thread.ofVirtual();
+        var t1 = builder.unstarted(() -> demo(1));
+        var t2 = builder.unstarted(() -> demo(2));
+
+        t1.start();
+        t2.start();
+
+        CommonUtils.sleep(Duration.ofSeconds(5));
+    }
+
+    private static void demo(int threadNumber) {
+        log.info("thread-{} started", threadNumber);
+        for (int i = 0; i < 10; i++) {
+            log.info("thread-{} is pringting {}. Thread {}", threadNumber, i, Thread.currentThread());
+            Thread.yield(); // we are forcefully giving the chance to other thread so that they can execute
+        }
+        log.info("thread-{} ended", threadNumber);
     }
 }
